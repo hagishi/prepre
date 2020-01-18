8adb12b01f96d8aa23c8613f5931430eb7b5ecac
hagishi <fujiyama@kakumei.jp>
2020-01-18 13:17:17 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import  from "react-";
+
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Helmet from "react-helmet";
+
+const About = () => {
+  return <div>
+    <Helmet>
+      <title>aaaaaa</title>
+    </Helmet>
+    <Link to="/">back</Link>
+    <div>eeeeeee</div>
+  </div>
+}
+
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route path="/about" component={About}/>
+      <Route path="/" component={App}/>
+    </Switch>
+  </Router>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
